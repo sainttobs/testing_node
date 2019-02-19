0107486cad647308b25159cbe54feaf2a6104288
Adeyefa Oluwatoba <sainttobs@gmail.com>
2019-02-19 02:29:10 +0100

    comparing values

--- chai-test.js
+++ chai-test.js
@@ -5,8 +5,17 @@ describe('chai test', ()=>{
 	it('should compare some values', ()=>{
 		expect(1).to.equal(1);
 	});
-
+	//how to use expect to check values
 	it('should do something else', ()=>{
 		expect({name:"foo"}).to.deep.equal({name:"foo"});
+		expect({name:"foo"}).to.have.property('name').to.equal('foo');
+		expect(5 > 8).to.be.false;
+		expect({}).to.be.a('object');
+		expect(3).to.be.a('number');
+		expect('bar').to.be.a('string').with.lengthOf(3);
+		expect([1,2,3].length).to.equal(3);
+		expect(null).to.be.null;
+		expect(undefined).to.not.exist;
+		expect(1).to.exist;
 	})
 })
